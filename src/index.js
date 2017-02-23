fe5abdf23dc5000daa79e6aec4e74d8f4c237b57
Weston Novelli <weston.novelli@civitaslearning.com>
2017-02-23 11:35:09 -0600

    more mobile compatibility

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, false);
+
+var lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
